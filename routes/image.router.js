cb94f9f8f92e295af26da4326e42cea02bcfe965
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 10:33:06 +0100

    update the code to use post method instead of get methid

--- routes/image.router.js
+++ routes/image.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const image = require("../controllers/image.controller");
 
-router.route("/").get(image.upload)
+router.route("/").post(image.upload)
 
 module.exports = router
