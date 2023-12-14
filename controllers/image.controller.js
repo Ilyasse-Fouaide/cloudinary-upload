a6233f7dc747062ec9f29c5e16d7b93a48babe73
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 11:10:12 +0100

    upload the image the specific destination

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,14 +1,19 @@
 const { StatusCodes } = require("http-status-codes");
 const tryCatchWrapper = require("../tryCatchWrapper");
 const path = require("path");
+const { badRequestError } = require("../customError");
 
 module.exports.upload = tryCatchWrapper(async (req, res, next) => {
   const { image } = req.files;
 
   // return a string `../public/upload/images/name.jpg`
-  const pathName = path.join("../public/upload/images", image.name);
+  const destination = path.join("../public/upload/images", image.name);
 
-  console.log(pathName);
+  image.mv(destination, (err) => {
+    if (err) {
+      return next(err)
+    }
 
-  res.status(StatusCodes.OK).json({ success: true })
+    res.status(StatusCodes.OK).json({ success: true })
+  })
 })
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -18,6 +18,7 @@ const errorHandler = (err, req, res, next) => {
     return res.status(err.status).json({
       success: false,
       error: {
+        status: err.status,
         message: err.message
       }
     })
