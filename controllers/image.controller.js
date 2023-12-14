03d9451e507a1d7611ba632970f5357329c353c9
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 11:03:50 +0100

    access to the req.file name

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,6 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
 const tryCatchWrapper = require("../tryCatchWrapper");
+const path = require("path");
 
 module.exports.upload = tryCatchWrapper(async (req, res, next) => {
+  const { image } = req.files;
+
+  // return a string `../public/upload/images/name.jpg`
+  const pathName = path.join("../public/upload/images", image.name);
+
+  console.log(pathName);
+
   res.status(StatusCodes.OK).json({ success: true })
 })
