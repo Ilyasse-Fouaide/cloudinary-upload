3bc3ac1e94f74b712159a3fc77282bbcdb0476ab
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 09:33:59 +0100

    connect to the db successfuly

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require("./config");
 const productRouter = require("./routes/product.router");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
+const connect = require("./db")
 
 const app = express();
 
@@ -12,8 +13,9 @@ app.use(errorHandler);
 
 const port = config.APP_PORT || 5001;
 
-const start = () => {
+const start = async () => {
   try {
+    await connect(config.DB_URI);
     app.listen(port, () => console.log(`listening to the port ${port}...`))
   } catch (error) {
     throw new Error(error.message);
