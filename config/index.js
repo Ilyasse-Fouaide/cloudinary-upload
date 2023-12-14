f8ac3f26f2118bf6db02e805795cf352f3c3a556
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 13:30:41 +0100

    Add Cloudinary environment variables

--- config/index.js
+++ config/index.js
@@ -4,4 +4,7 @@ dotenv.config();
 module.exports = {
   "APP_PORT": process.env.APP_PORT,
   "DB_URI": process.env.DB_URI,
+  "CLOUD_NAME": process.env.CLOUD_NAME,
+  "CLOUD_API_KEY": process.env.CLOUD_API_KEY,
+  "CLOUD_API_SECRET": process.env.CLOUD_API_SECRET,
 }
