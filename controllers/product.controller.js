11fbb4f49e2a4d26b6cad928802314890c12b985
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 10:02:42 +0100

    add create product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,15 @@
 const { StatusCodes } = require("http-status-codes");
 const tryCatchWrapper = require("../tryCatchWrapper");
+const Product = require("../models/product.model");
 
 module.exports.index = tryCatchWrapper(async (req, res, next) => {
+  res.status(StatusCodes.OK).json({ success: true })
+});
+
+module.exports.create = tryCatchWrapper(async (req, res, next) => {
+  const { name, price, image } = req.body;
+
+  await Product.create({ name, price, image });
+
   res.status(StatusCodes.OK).json({ success: true })
 });
