7febf31aa0ce8cafcc4435b9b405ab9440dc21b8
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 14:48:10 +0100

    use the old upload image

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,50 +1,51 @@
-const { v2: cloudinary } = require("cloudinary");
 const { StatusCodes } = require("http-status-codes");
+const { v2: cloudinary } = require("cloudinary");
+const fs = require("fs");
 const tryCatchWrapper = require("../tryCatchWrapper");
 const path = require("path");
 const { badRequestError } = require("../customError");
-const fs = require("fs");
-
-// module.exports.upload = tryCatchWrapper(async (req, res, next) => {
-//   if (!req.files) {
-//     return next(badRequestError("Path `image` is required!."))
-//   }
-
-//   const image = req.files.image;
-
-//   if (image.mimetype.split("/")[0] !== "image") {
-//     return next(badRequestError(`Invalid file type "${image.name}" provided.`));
-//   }
-
-//   if (image.size > 700000) {  // 700kb
-//     return next(badRequestError("Image size exceeds the limit of 700kb"));
-//   }
-
-//   // return a string `__dirname + ../public/upload/images/name.jpg`
-//   const destination = path.join(__dirname, "../public/upload/images", image.name);
-
-//   image.mv(destination, (err) => {
-//     if (err) {
-//       return next(err)
-//     }
-
-//     res.status(StatusCodes.OK).json({
-//       success: true, file: `/upload/images/${image.name}`
-//     })
-//   })
-// })
 
 module.exports.upload = tryCatchWrapper(async (req, res, next) => {
-  const uploadedImage = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
-    use_filename: true,
-    folder: "upload-file"
-  });
+  // TODO : grap this to image middleware
+  if (!req.files) {
+    return next(badRequestError("Path `image` is required!."))
+  }
 
-  // we store the image uploaded temporary then we delete it
-  fs.unlinkSync(req.files.image.tempFilePath)
+  const image = req.files.image;
 
-  res.status(StatusCodes.OK).json({
-    success: true,
-    secure_url: uploadedImage.secure_url
+  if (image.mimetype.split("/")[0] !== "image") {
+    return next(badRequestError(`Invalid file type "${image.name}" provided.`));
+  }
+
+  if (image.size > 700000) {  // 700kb
+    return next(badRequestError("Image size exceeds the limit of 700kb"));
+  }
+
+  // return a string `__dirname + ../public/upload/images/name.jpg`
+  const destination = path.join(__dirname, "../public/upload/images", image.name);
+
+  image.mv(destination, (err) => {
+    if (err) {
+      return next(err)
+    }
+
+    res.status(StatusCodes.OK).json({
+      success: true, file: `/upload/images/${image.name}`
+    })
   })
 })
+
+// module.exports.upload = tryCatchWrapper(async (req, res, next) => {
+//   const uploadedImage = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+//     use_filename: true,
+//     folder: "upload-file"
+//   });
+
+//   // we store the image uploaded temporary then we delete it
+//   fs.unlinkSync(req.files.image.tempFilePath)
+
+//   res.status(StatusCodes.OK).json({
+//     success: true,
+//     secure_url: uploadedImage.secure_url
+//   })
+// })
