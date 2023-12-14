993c7faed5e538480225646fc9d132b9a55b2388
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-14 10:31:24 +0100

    update the code to us api/v1/products

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", productRouter);
+app.use("/api/v1/products", productRouter);
 app.use("/api/v1/images", imageRouter);
 app.use(notFound);
 app.use(errorHandler);
